d2e9dcc681af34354f76e02f1247112b1a0284ce
Shikha Dixit <95029830+Shixit@users.noreply.github.com>
2023-03-31 23:00:35 +0530

    Create 26. Remove Duplicates from Sorted Array.cpp

---
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+            set<int> unique;
+            for(int i=0;i<nums.size();i++)
+            {
+                unique.insert(nums[i]);
+            }
+            nums.assign(unique.begin(), unique.end());
+            return unique.size();
+    }
+};
