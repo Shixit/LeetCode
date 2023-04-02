2c93ae0dc87b1cc3383c0ca816460f57343e7acc
Shikha Dixit <95029830+Shixit@users.noreply.github.com>
2023-04-02 17:35:49 +0530

    Create 205. Isomorphic Strings.cpp

---
+++ 205. Isomorphic Strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool solve(string s, string t)
+    {
+         map<char,char> mp;
+            for(int i=0;i<s.length();i++)
+            {
+                if(mp.find(s[i]) != mp.end())
+                {
+                    if(mp[s[i]] != t[i])
+                        return false;
+                }
+                else
+                    {
+                        mp[s[i]] = t[i];    
+                    } 
+            }
+            return true;
+    }
+    bool isIsomorphic(string s, string t) 
+    {
+        if(s.length() != t.length())
+            return false;
+
+           return solve(s,t) && solve(t,s);
+    }
+};
